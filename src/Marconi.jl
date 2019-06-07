979ae42262314d064572267deb96e6f298bad460
Kiran Shila <kiranshila@Kirans-MacBook-Pro.local>
2019-06-07 12:33:10 -0400

    Added content to readme, implemented reading touchstone

--- src/Marconi.jl
+++ src/Marconi.jl
@@ -1,9 +1,191 @@
 module Marconi
+using InspectDR # For Smith Charts
+include("NetworkParameters.jl")
 
-export pi_approximation
 
-function pi_approximation()
-  return 22.0 / 7.0
+# Package exports
+export readTouchstone
+
+# Export Network Parameters
+export y2s
+export z2s
+export h2s
+export g2s
+
+"""
+The base Network type for representing n-port linear networks with characteristic impedance Z0.
+  By default, the network is stored as S-Parameters with the corresponding frequency list.
+"""
+mutable struct Network
+  ports::Int
+  Z0::Union{Real,Complex}
+  frequency::Array{Real,1}
+  s_params::Array{Array{Union{Real,Complex},2},1}
+  passive::Bool
+  reciprocal::Bool
+  isEquation::Bool
+  equation::Function
 end
 
+# File option enums
+@enum paramType S Y Z G H
+@enum paramFormat MA DB RI
+
+"""
+    readTouchstone("myFile.sNp")
+
+Reads the contents of `myFile.sNp` into a Network object.
+This will convert all file types to S-Parameters, Real/Imaginary
+
+Currently does not support reference lines (Different port impedances) or noise parameters
+"""
+function readTouchstone(filename::String)
+  # File option settings - defaults
+  thisfreqExponent = 1e9
+  thisParamType = S
+  thisParamFormat = MA
+  thisZ0 = 50.
+
+  # Setup blank network object to build from
+  thisNetwork = Network(0,0,[],[],false,false,false,nothing)
+
+  # Open the file
+  open(filename) do f
+    while !eof(f)
+      line = readline(f)
+      if line[1] == '!' # Ignore comment lines
+        continue
+      elseif line[1] == '#' # Parse option line
+        # Option line contains [HZ/KHZ/MHZ/GHZ] [S/Y/Z/G/H] [MA/DB/RI] [R n]
+        # Or contains nothing implying GHZ S MA R 50
+        options = line[2:end]
+        if length(options) == 0
+          continue # Use defaults
+        else
+          options = split(strip(options)," ")
+          # Some VNAs put random amounts of spaces between the options,
+          # so we have to remove all the empty entries
+          options = [option for option in options if option != ""]
+
+          # Process frequency exponent
+          if options[1] == "HZ"
+            thisfreqExponent = 1.
+          elseif options[1] == "KHZ"
+            thisfreqExponent = 1e3
+          elseif options[1] == "MHZ"
+            thisfreqExponent = 1e6
+          elseif options[1] == "GHZ"
+            thisfreqExponent = 1e9
+          end
+
+          # Process Parameter Type
+          if options[2] == "S"
+            thisParamType = S
+          elseif options[2] == "Y"
+            thisParamType = Y
+          elseif options[2] == "Z"
+            thisParamType = Z
+          elseif options[2] == "G"
+            thisParamType = G
+          elseif options[2] == "H"
+            thisParamType = H
+          end
+
+          # Process Parameter Format
+          if options[3] == "MA"
+            thisParamFormat = MA
+          elseif options[3] == "DB"
+            thisParamFormat = DB
+          elseif options[3] == "RI"
+            thisParamFormat = RI
+          end
+
+          # Process Z0
+          thisZ0 = parse(Float64,options[5])
+        end
+      else # Process everything else
+        freq, ports, params = processTouchstoneLine(line,thisfreqExponent,thisParamType,thisParamFormat,thisZ0)
+        thisNetwork.ports = ports
+        thisNetwork.Z0 = thisZ0
+        push!(thisNetwork.frequency,freq)
+        push!(thisNetwork.s_params,params)
+      end
+    end
+  end
+
+  # Checks for passivisity and recipocity
+  thisNetwork.passive = isPassive(thisNetwork)
+  thisNetwork.reciprocal = isReciprocal(thisNetwork)
+
+  # Return the constructed network
+  return thisNetwork
 end
+
+"Internal function to process touchstone lines"
+function processTouchstoneLine(line::String,freqExp::Real,paramT::paramType,paramF::paramFormat,Z0::T) where {T <: Number}
+  lineParts = split(line," ")
+  frequency = parse(Float64,lineParts[1]) * freqExp
+  ports = √((length(lineParts)-1)/2) # Parameters are in two parts for each port
+  if mod(ports,1) != 0
+    throw(DimensionMismatch("Parameters in file are not square, somethings up"))
+  end
+  ports = floor(Int,ports) # It needs to be an Int anyway
+
+  # Step 1, get the parameters into RI format as that's what we will use
+  params = zeros(Complex,ports,ports)
+  for i = 2:2:(ports*ports*2) # Skip frequency
+    # There will be ports*ports number of parameters
+    paramIndex = floor(Int,i/2)
+    if paramF == RI # Real Imaginary
+      # Do nothing, already in the right type
+      params[paramIndex] = parse(Float64,lineParts[i]) + 1.0im * parse(Float64,lineParts[i+1])
+    elseif paramF == MA # Magnitude Angle(Degrees)
+      mag = parse(Float64,lineParts[i])
+      angle = parse(Float64,lineParts[i+1])
+      params[paramIndex] = mag  * cosd(angle) +
+                           1.0im * mag  * sind(angle)
+    elseif paramF == DB # dB Angle
+      mag = 10^(parse(Float64,lineParts[i])/20)
+      angle = parse(Float64,lineParts[i+1])
+      params[paramIndex] = mag  * cosd(angle) +
+                           1.0im * mag  * sind(angle)
+    end
+  end
+
+  # Step 2, convert into S-Parameters
+  if paramT == S
+    # Do nothing, they are already S
+  elseif paramT == Z
+    params = z2s(params,Z0=Z0)
+  elseif paramT == Y
+    params = y2s(params,Z0=Z0)
+  end # TODO H and G Parameters
+
+  return frequency,ports,params
+end
+
+"Internal function to check for pasivisity"
+function isPassive(network::Network)
+  for parameter in network.s_params
+    for s in parameter
+      if abs(s) > 1
+        return false
+      end
+    end
+  end
+  # If we got through everything, then it's passive
+  return true
+end
+
+"Internal function to check for recipocity"
+function isReciprocal(network::Network)
+  #FIXME
+  return true
+end
+
+function plotSmith(network::Network,)
+
+end
+
+
+end # Module End
---
+++ src/NetworkParameters.jl
@@ -0,0 +1,70 @@
+using LinearAlgebra
+
+"""
+        y2s(y)
+
+Converts Y-Parameters `y` to S-Parameters. Optionally include reference
+impedance with kwarg `Z0` with `y2s(y,Z0=50)`.
+
+# Example
+```julia-repl
+y11 =  0.0488133074245012 - 0.390764155450191im
+y12 = -0.0488588365420561 + 0.390719345880018im
+y21 = -0.0487261119282660 + 0.390851884427087im
+y22 =  0.0487710062903760 - 0.390800401433241im
+
+y_params = [y11 y12; y21 y22]
+
+julia> y2s(y_params)
+2×2 Array{Complex{Float64},2}:
+ 0.00381839+0.0247966im    0.996111-0.0249991im
+   0.996392-0.0253812im  0.00374364+0.0249161im
+
+```
+"""
+function y2s(y::Array{T,2};Z0::Number=50.) where {T <: Number}
+    # We have to downcast to complex because of type weirdness in Diagonal
+    Gref = Diagonal([1/√(abs(real(Z0))) for i in 1:size(y)[1]])
+    Zref = Diagonal([Z0 for i in 1:size(y)[1]])
+
+    return Gref * (convert(Array{Complex{Float64},2},I - Zref * y)) *
+                  (convert(Array{Complex{Float64},2},I + Zref * y))^-1 *
+                  complex(Gref)^-1
+end
+
+"""
+        z2s(z)
+
+Converts Z-Parameters `z` to S-Parameters. Optionally include reference
+impedance with kwarg `Z0` with `z2s(z,Z0=50)`.
+
+# Example
+```julia-repl
+z11 = -14567.2412789287 - 148373.315116592im
+z12 = -14588.1106171651 - 148388.583516562im
+z21 = -14528.0522132692 - 148350.705757767im
+z22 = -14548.5996561832 - 148363.457002006im
+
+z_params = [z11 z12; z21 z22]
+
+julia> z2s(z_params)
+2×2 Array{Complex{Float64},2}:
+ 0.00381839+0.0247966im    0.996392-0.0253812im
+   0.996111-0.0249991im  0.00374364+0.0249161im
+
+```
+"""
+function z2s(z::Array{T,2};Z0::Number=50.) where {T <: Number}
+    Gref = Diagonal([1/√(abs(real(Z0))) for i in 1:size(z)[1]])
+    Zref = Diagonal([Z0 for i in 1:size(z)[1]])
+
+    return Gref * (convert(Array{Complex{Float64},2},z - Zref)) *
+                  (convert(Array{Complex{Float64},2},z + Zref))^-1 *
+                  complex(Gref)^-1
+end
+
+function h2s(h::Array{Number,2};Z0::Number=50.)
+end
+
+function g2s(g::Array{Number,2};Z0::Number=50.)
+end
