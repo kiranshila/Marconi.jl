e07ade99c00195519acdb8faa1002538185e9251
Kiran Shila <kiranshila@mail.usf.edu>
2019-06-12 23:47:27 -0400

    more travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,3 @@
 using Marconi, Test
 
 include("test_networkanalysis.jl")
-
-# Build the docs on Julia v1.0
-if get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.0"
-    cd(joinpath(@__DIR__, "..")) do
-        withenv("JULIA_LOAD_PATH" => nothing) do
-            cmd = `$(Base.julia_cmd()) --depwarn=no --color=yes --project=docs/`
-            coverage = Base.JLOptions().code_coverage == 0 ? "none" : "user"
-            run(`$(cmd) -e 'using Pkg; Pkg.instantiate()'`)
-            run(`$(cmd) --code-coverage=$(coverage) docs/make.jl`)
-        end
-    end
-end
--- test/test_networkanalysis.jl
+++ test/test_networkanalysis.jl
@@ -1,3 +1,10 @@
 @testset "Converting between network parameters" begin
-    
+    # To and from z
+    z11 = -14567.2412789287 - 148373.315116592im
+    z12 = -14588.1106171651 - 148388.583516562im
+    z21 = -14528.0522132692 - 148350.705757767im
+    z22 = -14548.5996561832 - 148363.457002006im
+    z_params = [z11 z12; z21 z22]
+    s = z2s(z_params)
+    @test z_params ≈ s2z(s) # Due to float error
 end
