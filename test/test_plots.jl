04fcab59136f09a6b4a1f5de18cb0127f8ca7ce4
Kiran Shila <kiranshila@mail.usf.edu>
2019-06-17 17:11:43 -0400

    Added plot tests

--- test/test_plots.jl
+++ test/test_plots.jl
@@ -9,4 +9,9 @@
     bpf = readTouchstone("../examples/BPF.s2p")
     sc2 = plotSmithData(bpf,(1,1))
     @test length(sc2.contents) == 1
+    # Check the input validation
+    @test_throws DomainError plotSmithData(bpf,(3,1))
+    @test_throws DomainError plotSmithData(bpf,(1,3))
+    @test_throws DomainError plotSmithData(bpf,(-1,1))
+    @test_throws DomainError plotSmithData(bpf,(1,-1))
 end
