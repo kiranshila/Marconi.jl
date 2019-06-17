c9d1d57bde54f39478481914835bf075cd88c4c8
Kiran Shila <kiranshila@mail.usf.edu>
2019-06-17 10:48:26 -0400

    Testing coverage stuff

--- examples/NetworkTest.jl
+++ examples/NetworkTest.jl
@@ -1,14 +1,16 @@
 using Marconi
 using PGFPlotsX
 
-bpf = readTouchstone("examples/Amp.s2p")
+bpf = readTouchstone("examples/Short.s1p")
 
 style1 = @pgf {color = "red", "thick"}
 style2 = @pgf {color = "blue", "thick"}
 style3 = @pgf {color = "green", "thick"}
 style4 = @pgf {color = "yellow", "thick"}
 
-ax = plotRectangular(bpf,(1,1),dB,opts = style1)
+plotSmithData(bpf,(1,1),opts=style1)
+
+ax = plotRectangular(bpf,(1,1),imag,opts = style1)
 plotRectangular!(ax,bpf,(1,1),dB,opts = style2)
 plotRectangular!(ax,bpf,(1,1),dB,opts = style3)
 plotRectangular!(ax,bpf,(1,2),dB,opts = style4)
