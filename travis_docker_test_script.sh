11c1ac89288ac3f8adc700164ed0c2dbaf2c8d9e
Kiran Shila <me@kiranshila.com>
2019-10-02 12:51:06 -0400

    Travis

--- travis_docker_test_script.sh
+++ travis_docker_test_script.sh
@@ -11,5 +11,5 @@ TESTCMD=xvfb-run $JULIABIN
 cd /mnt && if [[ -a .git/shallow ]]; then git fetch --unshallow; fi
 
 # run tests
-$TESTCMD --color=yes -e "import Pkg; Pkg.build(); Pkg.test(; coverage=true)"
+$TESTCMD -e "import Pkg; Pkg.build(); Pkg.test(; coverage=true)"
 chmod 777 Manifest.toml
